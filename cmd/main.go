38833c2896965101e59d8ab8e8fcf29af534dadc
Eduard Arutyunyan <e.arutyunyan08@gmail.com>
2024-06-12 18:50:40 +0500

    added command line arguments support

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var START_DATE = time.Date(2024, 06, 06, 0, 0, 0, 0, time.Now().Location())
+// var START_DATE = time.Date(2024, 05, 1, 0, 0, 0, 0, time.Now().Location())
+// var END_DATE time.Time = time.Date(2024, 05, 31, 0, 0, 0, 0, time.Now().Location())
 
 func main() {
 	err := godotenv.Load()
@@ -19,6 +20,24 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		return
+	}
+
+	var arguments = map[int]time.Time{
+		0: time.Now(),
+	}
+
+	for i, arg := range os.Args[1:] {
+		arguments[i], err = time.Parse("2006-01-02", arg)
+
+		if err != nil {
+			log.Fatal("Date should be formatted as YYYY-MM-DD")
+			return
+		}
+
+		if i == 1 && arguments[i].Before(arguments[0]) {
+			log.Fatal("Dates must be in ascending order")
+			return
+		}
 	}
 
 	apiUrls := strings.Split(os.Getenv("API_URL"), ",")
@@ -31,19 +50,29 @@ func main() {
 		Password: apiKey,
 	}
 
-	sumInHours := 0.00
+	search := fmt.Sprintf(
+		"worklogAuthor = %s and worklogDate >= %s",
+		workLogUserId, arguments[0].Format("2006-01-02"),
+	)
 
-	fmt.Println("WORKLOG SINCE:", START_DATE.Format(time.RFC1123))
+	fmt.Println("WORKLOG SINCE:", arguments[0].Format(time.RFC1123))
 
-	for _, v := range apiUrls {
-		client, err := jira.NewClient(tp.Client(), v)
+	if endDate, ok := arguments[1]; ok {
+		search = search + fmt.Sprintf(" and worklogDate <= %s", endDate.Format("2006-01-02"))
+		fmt.Println("UNTIL:", endDate.Format(time.RFC1123))
+	}
+
+	sumInSeconds := 0.00
+
+	for _, url := range apiUrls {
+		client, err := jira.NewClient(tp.Client(), url)
 
 		if err != nil {
 			log.Fatal("Failed create a client")
 			return
 		}
 
-		issues, _, err := client.Issue.Search(fmt.Sprintf("worklogAuthor = %s and worklogDate >= %s", workLogUserId, START_DATE.Format("2006-01-02")), &jira.SearchOptions{})
+		issues, _, err := client.Issue.Search(search, &jira.SearchOptions{})
 
 		if err != nil {
 			log.Fatal("Failed fetch a board", err.Error())
@@ -60,19 +89,30 @@ func main() {
 					continue
 				}
 
-				if time.Time(*wl.Started).After(START_DATE) { // logged for the date (not when you logged, but for the date you logged)
+				started := time.Time(*wl.Started)
+				endDate, endDateDefined := arguments[1]
+
+				isBefore := true
+
+				if endDateDefined {
+					isBefore = started.Before(endDate)
+				}
+
+				if started.After(arguments[0]) && isBefore { // logged for the date (not when you logged, but for the date you logged)
 					length++
 					fmt.Println(issue.Key, time.Time(*wl.Started).Local().Format(time.RFC1123), "Author:", wl.Author.DisplayName)
 					fmt.Println(wl.Comment)
 					fmt.Println("TIME SPENT:", wl.TimeSpent)
 					fmt.Println("______________________________________________________")
 
-					sumInHours += float64(wl.TimeSpentSeconds) / 60 / 60
+					sumInSeconds += float64(wl.TimeSpentSeconds)
 				}
 
 			}
 		}
 	}
 
-	fmt.Printf("\nTotal: %dh %.0fm \n", int(sumInHours), 60*(sumInHours-float64(int(sumInHours))))
+	duration := time.Duration(sumInSeconds * float64(time.Second))
+
+	fmt.Printf("\nTotal: %.0fh %.0fm \n", duration.Hours(), duration.Hours()*60-duration.Minutes())
 }
