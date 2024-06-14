92864507bc760db833959472986019ec4ecbb39a
Eduard Arutyunyan <e.arutyunyan08@gmail.com>
2024-06-14 17:49:23 +0500

    pretty output

--- pkg/services/outputService.go
+++ pkg/services/outputService.go
@@ -18,7 +18,7 @@ const WORKLOG_STARTED_DATE_FORMAT = "02 Jan 2006 15:04 (Timezone: MST)"
 
 func (output *OutputService) FormattedStdOutString(issue jira.Issue, wl jira.WorklogRecord) string {
 	outputStringsArray := []string{
-		fmt.Sprintf("%s %s Author: %s", issue.Key, time.Time(*wl.Started).Local().Format(WORKLOG_STARTED_DATE_FORMAT), wl.Author.DisplayName),
+		fmt.Sprintf("Ticket: %s, Date: %s, Author: %s", issue.Key, time.Time(*wl.Started).Local().Format(WORKLOG_STARTED_DATE_FORMAT), wl.Author.DisplayName),
 		fmt.Sprintf("COMMENT: %s", wl.Comment),
 		fmt.Sprintf("TIME SPENT: %s", wl.TimeSpent),
 		"______________________________________________________\n\n",
